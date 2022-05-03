f84f1de7fe84ab087df93dabd8e265dd6784be91
nakedthunder <nakedthunder00@gmail.com>
2022-05-03 22:58:31 +0900

    update vue-cli

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue' //SPA 컴포넌트 
 
 Vue.config.productionTip = false
 
 new Vue({
+  //app이라는 컴포넌트 파일
   render: h => h(App),
 }).$mount('#app')
+
+//컴포넌트 내용을 다른 파일로 불러와서 h(App)으로 집어넣고 render를 했다. 
---
+++ vue-cli/src/testA.vue
@@ -0,0 +1,24 @@
+<!--SPA 
+    화면에 영역을 나눴을때 특정 영역에 해당하는 HTML, js, style을
+    한 파일에 관리하는 것이 SPA이다. Vue CLI를 통해 생성했다. 
+-->
+<template>
+    <!--HTML-->
+    <div>header</div>
+</template>
+
+<script>
+//원래는 new Vue({})를 생성해줬던 곳 
+export default {
+    //인스턴스 옵션 , props를 내려서 사용함 
+    methods: {
+        addNum : function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
